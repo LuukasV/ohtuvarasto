58aec248a951e6d7ec1e3705cb513e4dda0866ce
lvvuolle <lvvuolle@jyu.fi>
2025-11-03 19:09:19 +0200

    Rikottu testitiedosto

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -6,6 +6,8 @@ class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
 
+    foo
+
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
