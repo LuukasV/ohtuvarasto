a957ec1530cc7f09bdcc82e6a85f2af9b28ee742
lvvuolle <lvvuolle@jyu.fi>
2025-11-08 13:49:36 +0200

    Muokattu ohjelmiston koodi noudattamaan kaikkia pylinti saantoja

--- src/index.py
+++ src/index.py
@@ -5,15 +5,29 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    after_creation(mehua, olutta)
+    get_olut(olutta)
+    set_mehu(mehua)
+
+    virhetilanne_test()
+    olut_testit(olutta)
+    mehu_testit(mehua)
+
+if __name__ == "__main__":
+    main()
+
+def after_creation(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def get_olut(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def set_mehu(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +36,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhetilanne_test():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,15 +46,11 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def olut_testit(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
-    mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
 
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
@@ -47,12 +58,14 @@ def main():
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def mehu_testit(mehua):
+    print(f"Mehuvarasto: {mehua}")
+    print("mehua.lisaa_varastoon(-666.0)")
+    mehua.lisaa_varastoon(-666.0)
+    print(f"Mehuvarasto: {mehua}")
+
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
-
-
-if __name__ == "__main__":
-    main()
